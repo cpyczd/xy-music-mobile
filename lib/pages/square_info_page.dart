2c096d9b91b7265cd5403dfa1b0c5320bc88ba0a
Chenzedeng <chenzedeng@geexfinance.com>
2021-06-18 18:10:11 +0800

    更新页面

---
+++ lib/pages/square_info_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+/*
+ * @Description: 
+ * @Author: cpyczd
+ * @Date: 2021-06-18 4:05 下午
+ * @LastEditTime: 
+ */class SquareInfoPage extends StatefulWidget {
+  SquareInfoPage({Key key}) : super(key: key);
+
+  @override
+  _SquareInfoPageState createState() => _SquareInfoPageState();
+}
+
+class _SquareInfoPageState extends State<SquareInfoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
